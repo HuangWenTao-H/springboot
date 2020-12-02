b5ed715913282016ec5e2d86128c3d89b5ec8258
tony <840262167@qq.com>
2020-12-02 13:45:28 +0800

    v1.0

---
+++ springBootDemo/src/main/java/com/springBoot/HelloWorldMainApplication.java
@@ -0,0 +1,11 @@
+package com.springBoot;
+
+
+/**
+ * @SpringBootApplication 来标注一个主程序类，说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    // Spring应用启动起来
+    SpringApplication.run(HelloWorldMainApplication.class,args);
+}
